1e3980f95d024759d5595bec72a7ad850594e3dd
kawouoderrick1er <kawouoderrick@gmail.com  git config --global user.name>
2024-07-18 10:01:06 +0200

    le premier commit

---
+++ app.js
@@ -0,0 +1,65 @@
+function palidrome(mots) {
+    const letter = mots.split('')
+    letter.reverse()
+    const ordre = letter.join('')
+}
+let 
+
+
+
+// ls.forEach(v => console.log(v)) il ulste tout les element
+// const ul = document.querySelector('ul')
+// ul.setAttribute('hidden','hidden') ajouter un attribut
+// ul.removeAttribute('hidden') supprimer un attribut
+// ul.classList.toggle ou .add('red') ajouter la classe rouge si elle nexiste et la suppr si el existe
+
+// const li = document.querySelector('li')
+// li.style.color = 'blue'
+    // // li.style.fontWeight='bolder'
+
+
+
+    // const newli =document.createElement('li')
+    // newli.innerHTML='salut mes fils'
+    // document.querySelector('ul').appendChild(a la fin du) ou prepene(au debut du navigateur)(newli)
+    // permet dajouter un element a travers js dans le navigateur
+    // const junior=document.querySelector('.junior')
+    // const orchel = document.createElement('p')
+    // orchel.innerHTML='chargement des doc...'
+    // // innertext = innerHTML
+    // junior.appendChild(orchel)
+
+
+
+//    const palidrome = document.querySelector('button')
+//    palidrome.addEventListener('click' , function() {
+//     const nom = document.querySelector('p').split('').reverse().join('')
+//      alert('nom')
+//     const no = document.querySelector('li')
+//     alert('no.split.reverse.join')
+//    });
+
+
+
+
+// // function estPalindrome(chaine) {
+// //     chaine = chaine.toLowerCase();
+// //      // Convertir en minuscules
+  
+// //     let miroir = chaine.split('').reverse().join('');
+// //      // Inverser la chaîne
+  
+// //     return chaine === miroir;
+// //   }
+// //   const slt = document.getElementById('salut');
+// //   const p = estPalindrome(chaine);
+
+
+  
+//   let chaine = prompt("Entrez une chaîne de caractères : ");
+  
+//   if (estPalindrome(chaine)) {
+//     console.log(chaine + " est un palindrome.");
+//   } else {
+//     console.log(chaine + " n'est pas un palindrome.");
+//   }
